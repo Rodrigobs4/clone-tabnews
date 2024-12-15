2e31450a39fb8ac0eec95d20f3ef672f3cc6aa47
Rodrigobs4 <133627785+Rodrigobs4@users.noreply.github.com>
2024-12-15 16:49:54 +0000

    evoluindo_nas_apis

---
+++ models/calculadora.js
@@ -0,0 +1,6 @@
+function somar (numerol, numero2)
+if (typeof numerol !== "number") {
+return "Erro";
+}
+return numerol + numero2;
+exports. somar = somar;
---
+++ pages/api/status.js
@@ -0,0 +1,5 @@
+function status(request, response) {
+    response.status(200).json({"Nome" : "Rodrigo"});
+}
+
+export default status
